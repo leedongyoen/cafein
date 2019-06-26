bc50a1796e61c69b45cde79887ab9554f799d51e
User <User@YD-PC-3-008>
2019-06-26 16:52:10 +0900

    0626 하기전

--- cafein/src/main/java/co/yedam/cafein/store/StoreController.java
+++ cafein/src/main/java/co/yedam/cafein/store/StoreController.java
@@ -1,9 +1,13 @@
 package co.yedam.cafein.store;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StoreController {
 	
-	
+	@RequestMapping("joinForm.do")
+	public String join() {
+		return "store/join";
+	}
 }
