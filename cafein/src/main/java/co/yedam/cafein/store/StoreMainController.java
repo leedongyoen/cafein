18140ce6a748bb6a6d3dee27bfacdd1819f6036b
user3 <User@User-PC>
2019-06-26 17:56:28 +0900

    매장 메인

--- cafein/src/main/java/co/yedam/cafein/store/StoreMainController.java
+++ cafein/src/main/java/co/yedam/cafein/store/StoreMainController.java
@@ -1,9 +1,33 @@
 package co.yedam.cafein.store;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StoreMainController {
+	
+	//매장 메인화면
+	@RequestMapping("storemain.do")
+	public String storemain() {
+		return "store/main";
+	}
+	//매장 우리매장 고객 조회
+	@RequestMapping("customerlist.do")
+	public String customerlist() {
+		return "store/customerlist";
+	}
+	
+	//매장 주문내역 조회
+	@RequestMapping("orderlist.do")
+	public String orderlist() {
+		return "store/orderlist";
+	}
+	
+	//매장 주문하기
+		@RequestMapping("pos.do")
+		public String pos() {
+			return "store/pos";
+		}
 	
 
 }
