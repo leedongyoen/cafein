7f3933245d30e91ece97ff271f38d3fc6a3c2dd2
User <User@YD-PC-3-008>
2019-06-27 11:12:45 +0900

    매장메인화면 컨트롤러 위치 변경

--- cafein/src/main/java/co/yedam/cafein/admin/AdminController.java
+++ cafein/src/main/java/co/yedam/cafein/admin/AdminController.java
@@ -1,8 +1,13 @@
 package co.yedam.cafein.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
-
+	// 관리자 메인 화면
+		@RequestMapping("adminMainForm.do")
+		public String adminMainForm() {
+			return "admin/main";
+		}
 }
