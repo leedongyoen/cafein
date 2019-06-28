0a0937ada1e36e8acfd9143cc5b01f1e34f6d14d
User <User@YD-PC-3-008>
2019-06-28 14:48:36 +0900

    나만의 메뉴, 주문상세 vo

---
+++ cafein/src/main/java/co/yedam/cafein/vo/MyMenuVO.java
@@ -0,0 +1,40 @@
+package co.yedam.cafein.vo;
+
+public class MyMenuVO {
+	
+	private String myNum;
+	private String cId;
+	private String mNum;
+	private String cuNum;
+	public String getMyNum() {
+		return myNum;
+	}
+	public void setMyNum(String myNum) {
+		this.myNum = myNum;
+	}
+	public String getcId() {
+		return cId;
+	}
+	public void setcId(String cId) {
+		this.cId = cId;
+	}
+	public String getmNum() {
+		return mNum;
+	}
+	public void setmNum(String mNum) {
+		this.mNum = mNum;
+	}
+	public String getCuNum() {
+		return cuNum;
+	}
+	public void setCuNum(String cuNum) {
+		this.cuNum = cuNum;
+	}
+	@Override
+	public String toString() {
+		return "MyMenuVO [myNum=" + myNum + ", cId=" + cId + ", mNum=" + mNum + ", cuNum=" + cuNum + "]";
+	}
+	
+	
+
+}
---
+++ cafein/src/main/java/co/yedam/cafein/vo/OrderDetailsVO.java
@@ -0,0 +1,39 @@
+package co.yedam.cafein.vo;
+
+public class OrderDetailsVO {
+
+	private String oDnum;
+	private String oNum;
+	private String mNum;
+	private String oQty;
+	public String getoDnum() {
+		return oDnum;
+	}
+	public void setoDnum(String oDnum) {
+		this.oDnum = oDnum;
+	}
+	public String getoNum() {
+		return oNum;
+	}
+	public void setoNum(String oNum) {
+		this.oNum = oNum;
+	}
+	public String getmNum() {
+		return mNum;
+	}
+	public void setmNum(String mNum) {
+		this.mNum = mNum;
+	}
+	public String getoQty() {
+		return oQty;
+	}
+	public void setoQty(String oQty) {
+		this.oQty = oQty;
+	}
+	@Override
+	public String toString() {
+		return "OrderDetailsVO [oDnum=" + oDnum + ", oNum=" + oNum + ", mNum=" + mNum + ", oQty=" + oQty + "]";
+	}
+	
+	
+}
