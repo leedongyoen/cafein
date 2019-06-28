eb9d6eb04a02bb4fe75072f3268ae6af4373ef9d
User <User@YD-PC-004>
2019-06-28 15:29:23 +0900

    ㅎㅎㅎㅎㅎ 보 수정 AN

--- cafein/src/main/java/co/yedam/cafein/vo/MenuStockStatusVO.java
+++ cafein/src/main/java/co/yedam/cafein/vo/MenuStockStatusVO.java
@@ -5,5 +5,31 @@ public class MenuStockStatusVO {
 	private String statNum;
 	private String msStatus;
 	private String msNum;
+	public String getStatNum() {
+		return statNum;
+	}
+	public void setStatNum(String statNum) {
+		this.statNum = statNum;
+	}
+	public String getMsStatus() {
+		return msStatus;
+	}
+	public void setMsStatus(String msStatus) {
+		this.msStatus = msStatus;
+	}
+	public String getMsNum() {
+		return msNum;
+	}
+	public void setMsNum(String msNum) {
+		this.msNum = msNum;
+	}
+	@Override
+	public String toString() {
+		return "MenuStockStatusVO [statNum=" + statNum + ", msStatus=" + msStatus + ", msNum=" + msNum + "]";
+	}
 
+	
+	
+	
+	
 }
--- cafein/src/main/java/co/yedam/cafein/vo/MenuVO.java
+++ cafein/src/main/java/co/yedam/cafein/vo/MenuVO.java
@@ -8,5 +8,49 @@ public class MenuVO {
 	private String	sId;
 	private String	caNum;
 	private String	mStat;
+	public String getmNum() {
+		return mNum;
+	}
+	public void setmNum(String mNum) {
+		this.mNum = mNum;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public int getmPrice() {
+		return mPrice;
+	}
+	public void setmPrice(int mPrice) {
+		this.mPrice = mPrice;
+	}
+	public String getsId() {
+		return sId;
+	}
+	public void setsId(String sId) {
+		this.sId = sId;
+	}
+	public String getCaNum() {
+		return caNum;
+	}
+	public void setCaNum(String caNum) {
+		this.caNum = caNum;
+	}
+	public String getmStat() {
+		return mStat;
+	}
+	public void setmStat(String mStat) {
+		this.mStat = mStat;
+	}
+	@Override
+	public String toString() {
+		return "MenuVO [mNum=" + mNum + ", mName=" + mName + ", mPrice=" + mPrice + ", sId=" + sId + ", caNum=" + caNum
+				+ ", mStat=" + mStat + "]";
+	}
 
+	
+	
+	
 }
