11c0b5ccd3f0d1de99623d52dbe63dc67d4db228
User <User@YD-PC-3-015>
2019-07-03 18:11:40 +0900

    SalesAjaxController 추가 Salesday.jsp 수정

---
+++ cafein/src/main/java/co/yedam/cafein/store/sales/SalesAjaxController.java
@@ -0,0 +1,46 @@
+package co.yedam.cafein.store.sales;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import co.yedam.cafein.vo.SalesSumVO;
+
+@Controller
+public class SalesAjaxController {
+	@Autowired
+	SalesService salesService;
+	
+	@RequestMapping("/getsalesday.do")
+	@ResponseBody
+	public List<SalesSumVO> salesDayForm(SalesSumVO vo) {
+		return salesService.getDaySales(vo);
+	}
+	
+	@RequestMapping("/getsalesmenu.do")
+	@ResponseBody
+	public String salesMenuForm() {
+		return "store/salesmenu";
+	}
+	
+	@RequestMapping("/getsalestime.do")
+	@ResponseBody
+	public String salesTimeForm() {
+		return "store/salestime";
+	}
+	
+	@RequestMapping("/getsalesyear.do")
+	@ResponseBody
+	public String salesYearForm() {
+		return "store/salesyear";
+	}
+	
+	@RequestMapping("/getsalesmonth.do")
+	@ResponseBody
+	public String salesMonthForm() {
+		return "store/salesmonth";
+	}
+}
