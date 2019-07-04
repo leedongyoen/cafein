738e2768040abe65ea763d5fba0b2f617d8f3bdb
Hyuk <User@User-PC>
2019-07-04 14:51:38 +0900

    Add Stock(restCon/con/..)

---
+++ cafein/src/main/java/co/yedam/cafein/store/stock/StoreStockServiceImpl.java
@@ -0,0 +1,46 @@
+package co.yedam.cafein.store;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.yedam.cafein.vo.StockVO;
+
+@Service
+public class StoreStockServiceImpl implements StoreStockService {
+
+	@Autowired
+	StoreStockDAO stockDao;
+
+	@Override
+	public List<StockVO> getStockList(StockVO vo) {
+
+		return stockDao.getStockList(vo);
+
+	}
+
+	@Override
+	public int insertStock(StockVO vo) {
+		
+		return stockDao.insertStock(vo);
+	}
+
+	@Override
+	public int updateStock(StockVO vo) {
+		
+		return stockDao.updateStock(vo);
+	}
+
+	@Override
+	public int deleteStock(StockVO vo) {
+		
+		return stockDao.deleteStock(vo);
+	}
+
+	@Override
+	public StockVO getStock(StockVO vo) {
+		return stockDao.getStock(vo);
+	}
+
+}
