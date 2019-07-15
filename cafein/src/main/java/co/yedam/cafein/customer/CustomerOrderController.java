ba963b575dc2a1a4662f7f13364cf5538f6eb4b5
LeeJM <mossball1011@gmail.com>
2019-07-15 13:53:12 +0900

    파일 업로드 작업중

--- cafein/src/main/java/co/yedam/cafein/customer/CustomerOrderController.java
+++ cafein/src/main/java/co/yedam/cafein/customer/CustomerOrderController.java
@@ -31,11 +31,6 @@ public class CustomerOrderController {
 	
 	@Autowired
 	CustomerOrderServiceImpl service;
-	@Autowired
-	MenuServiceImpl service2;
-	@Autowired
-	RecipeSerciveImpl service3;
-
 	
 	// 주문으로 넘어가는 부분
 	  @RequestMapping(value="/customerorder",method=RequestMethod.POST) 
@@ -79,11 +74,49 @@ public class CustomerOrderController {
 		return "customer/orderdetails";
 	}
 	//고객장바구니 관리
+
+	  @RequestMapping(value="cartmng",method=RequestMethod.GET) public ModelAndView
+	  cartmng(ModelAndView mv, HttpSession session) {
+	  
+	 // ArrayList<CartVO> list = (ArrayList<CartVO>)session.getAttribute("cartlist");
+	 // for(int i = 0;i<list.size();i++) {
+	 // System.out.println(list.get(i).toString()); }
+	  
+	  
+	  RecipeVO vo = new RecipeVO();
+	  mv.addObject("optionname",service.getOptionName(vo));
+	  
+	  
+	  
+	  
+	  
+		/*
+		 * ArrayList<CartVO> list = (ArrayList<CartVO>)session.getAttribute("cartlist");
+		 * 
+		 * if(list == null) { list = new ArrayList<CartVO>(); list.add(vo); }else{
+		 * list.add(vo); }
+		 * 
+		 * session.setAttribute("cartlist", list);
+		 * System.out.println("session : "+session.getAttribute("cartlist"));
+		 * 
+		 */
+	  
+	  
+	  
+	  ArrayList<CartVO> list = (ArrayList<CartVO>)session.getAttribute("cartlist");
+	  if(list != null) {
+		  for(int i = 0;i<list.size();i++) {
+				System.out.println("-----------------------------------"+list.get(i).toString());
+			}  
+	  }else if(list == null) {
+		  System.out.println("없음");
+	  }
+	  
+	  mv.setViewName("customer/cartmng"); return mv; }
+	 
+	  
+	  
 	/*
-	 * @RequestMapping(cartmng.do) 
-	 * public String cartmng() { return "customer/cartmng"; }
-	 */
-	
 	@RequestMapping(value="cartmng",method=RequestMethod.GET)
 	public ModelAndView cartmng(ModelAndView mv, HttpSession session) {
 		//getSession CartVO를 꺼내고 
@@ -91,8 +124,7 @@ public class CustomerOrderController {
 		MenuVO vo = new MenuVO();
 		RecipeVO vo2 = new RecipeVO();
 		
-		mv.addObject("storemenu",service2.getMenuList(vo));
-		mv.addObject("storerecipe",service3.getRecipeDetailList(vo2));
+		
 		ArrayList<CartVO> list = (ArrayList<CartVO>)session.getAttribute("cartlist");
 		for(int i = 0;i<list.size();i++) {
 			System.out.println(list.get(i).toString());
@@ -101,8 +133,8 @@ public class CustomerOrderController {
 		mv.setViewName("customer/cartmng");
 		return mv;
 	}
-	// 
 	
+	*/
 	
 
 }
