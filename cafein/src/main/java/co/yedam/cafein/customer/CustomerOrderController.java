25d1533f7648e2351add00b5facc207f1812618d
LeeJM <mossball1011@gmail.com>
2019-07-18 09:11:46 +0900

    customerOrderController 나눔

--- cafein/src/main/java/co/yedam/cafein/customer/CustomerOrderController.java
+++ cafein/src/main/java/co/yedam/cafein/customer/CustomerOrderController.java
@@ -17,6 +17,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.JsonObject;
+
 import co.yedam.cafein.customer.order.CustomerOrderServiceImpl;
 import co.yedam.cafein.store.info.StoreInfoServiceImpl;
 import co.yedam.cafein.store.menu.MenuServiceImpl;
@@ -172,38 +176,5 @@ public class CustomerOrderController {
 		return "customer/orderdetails";
 	}
 
-	// 고객장바구니 관리
-	@RequestMapping(value = "cartmng", method = RequestMethod.GET)
-	public ModelAndView cartmng(ModelAndView mv, HttpSession session) {
-
-		// ArrayList<CartVO> list = (ArrayList<CartVO>)session.getAttribute("cartlist");
-		// for(int i = 0;i<list.size();i++) {
-		// System.out.println(list.get(i).toString()); }
-
-		RecipeVO vo = new RecipeVO();
-		StoreVO vo2 = new StoreVO();
-		MenuVO vo3 = new MenuVO();
-		mv.addObject("optionname", service.getOptionName(vo));
-		mv.addObject("storename", service2.getStoreList(vo2));
-		mv.addObject("menuimg", service3.getMenuList(vo3));
-		
-		mv.setViewName("customer/cartmng");
-		return mv;
-	}
-
-	/*
-	 * @RequestMapping(value="cartmng",method=RequestMethod.GET) public ModelAndView
-	 * cartmng(ModelAndView mv, HttpSession session) { //getSession CartVO를 꺼내고
-	 * //Mapper Query 돌아서 나머지 상세정보들을 들고와서 cartmng.jsp 보낸다 MenuVO vo = new MenuVO();
-	 * RecipeVO vo2 = new RecipeVO();
-	 * 
-	 * 
-	 * ArrayList<CartVO> list = (ArrayList<CartVO>)session.getAttribute("cartlist");
-	 * for(int i = 0;i<list.size();i++) {
-	 * System.out.println(list.get(i).toString()); }
-	 * 
-	 * mv.setViewName("customer/cartmng"); return mv; }
-	 * 
-	 */
 
 }
