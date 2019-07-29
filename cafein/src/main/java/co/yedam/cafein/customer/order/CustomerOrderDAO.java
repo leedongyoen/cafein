1555ff9433f34d8b43d340b4f4d02b84b09b9337
User <User@YD-PC-3-008>
2019-07-29 12:04:51 +0900

    임포트 수정

--- cafein/src/main/java/co/yedam/cafein/customer/order/CustomerOrderDAO.java
+++ cafein/src/main/java/co/yedam/cafein/customer/order/CustomerOrderDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;import org.springframework.core.annotation.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import co.yedam.cafein.vo.CustomerVO;
